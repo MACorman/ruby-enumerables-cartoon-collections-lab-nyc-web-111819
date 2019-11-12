b345b47c4f4db58175d7e362901b5b244b784971
Madeline Corman <madelinecorman@gmail.com>
2019-11-12 20:48:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,5 +13,5 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheeses = %w[cheese_types]
-  array.find {|any_cheese| any_cheese.include?(cheeses) }
+  array.find {|any_cheese| any_cheese.include?(cheeses)}
 end
