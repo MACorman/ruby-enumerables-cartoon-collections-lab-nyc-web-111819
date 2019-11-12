493ba89b6e0f6cf5a27809668613fdb3e49cb6fd
Madeline Corman <madelinecorman@gmail.com>
2019-11-12 20:52:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,14 +11,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
- # cheese_types = ["cheddar", "gouda", "camembert"]
-  #cheeses = %w[cheddar, gouda, camembert]
-  #array.find {|any_cheese| any_cheese.include?(cheeses)}
-  
-    cheeses = %w[gouda cheddar camembert]
-
-  array.find do |maybe_cheese|
-    cheeses.include?(maybe_cheese)
-  end
-  
+ cheese_types = ["cheddar", "gouda", "camembert"]
+ cheeses = %w[gouda cheddar camembert]
+ array.find {|any_cheese| cheeses.include?(any_cheese)}
 end
